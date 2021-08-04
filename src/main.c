bd2bc47da244b16f8cc4600d591451be6cb7e4ee
Ege Çetin <egecetin@hotmail.com.tr>
2021-08-04 19:12:27 +0300

    optional input

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     if (argc < 4)
     {
         printf("Usage ./kaleidoscope <Input Image Path> <Output Image Path> <N>\n");
+        printf("Usage ./kaleidoscope <Input Image Path> <Output Image Path> <N> <Dim constant> <Scale factor>\n");
         return retval;
     }
     printf("Start...\n");
@@ -15,19 +16,29 @@ int main(int argc, char *argv[])
     char *path = argv[1];
     char *outPath = argv[2];
     int n = atoi(argv[3]);
+    float k = 0.30;
+    float scaleDown = 0.45;
+    if (argc > 4)
+    {
+        k = atof(argv[4]);
+        scaleDown = atof(argv[5]);
+    }
 
     // Process
     ImageData imgData;
     printf("Reading %s... ", path);
-    retval = readImage(path, &imgData);
+    if ((retval = readImage(path, &imgData)))
+        return retval;
     printf(" %d\n", !retval);
 
     printf("Applying effect... ");
-    retval = kaleidoscope(&imgData, n, 0.30, 0.45);
+    if ((retval = kaleidoscope(&imgData, n, k, scaleDown)))
+        return retval;
     printf(" %d\n", !retval);
 
     printf("Saving %s... ", outPath);
-    retval = saveImage(outPath, &imgData);
+    if ((retval = saveImage(outPath, &imgData)))
+        return retval;
     printf(" %d\n", !retval);
 
     printf("Done...\n");
