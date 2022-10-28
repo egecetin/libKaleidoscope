763fc4cf0c0b2ff2ce4c38cb1bc960fee6cd017b
Ege Çetin <egecetin@hotmail.com.tr>
2022-10-28 16:26:37 +0000

    divide test

--- tests/imageDataTest.c
+++ tests/imageDataTest.c
@@ -2,10 +2,34 @@
 #include <kaleidoscope.h>
 #include <stdlib.h>
 
-START_TEST(ImageDataTest)
+START_TEST(ImageDataTest_Landscape)
 {
 	ImageData *imgPtr = NULL;
 	int width = 1024, height = 768, nComponents = 3;
+
+	// Init
+	imgPtr = (ImageData *)malloc(sizeof(ImageData));
+	ck_assert_ptr_nonnull(imgPtr);
+
+	// Check
+	ck_assert_int_eq(0, initImageData(imgPtr, width, height, nComponents));
+	ck_assert_int_eq(width, imgPtr->width);
+	ck_assert_int_eq(height, imgPtr->height);
+	ck_assert_int_eq(nComponents, imgPtr->nComponents);
+	ck_assert_ptr_nonnull(imgPtr->data);
+
+	// De-init
+	deInitImageData(imgPtr);
+	ck_assert_ptr_null(imgPtr->data);
+
+	free(imgPtr);
+}
+END_TEST
+
+START_TEST(ImageDataTest_Portrait)
+{
+	ImageData *imgPtr = NULL;
+	int width = 768, height = 1024, nComponents = 3;
 
 	// Init
 	imgPtr = (ImageData *)malloc(sizeof(ImageData));
@@ -34,7 +58,8 @@ Suite *imageDataTestSuite(void)
 	s = suite_create("ImageDataTest");
 	tc_core = tcase_create("Core");
 
-	tcase_add_test(tc_core, ImageDataTest);
+	tcase_add_test(tc_core, ImageDataTest_Portrait);
+	tcase_add_test(tc_core, ImageDataTest_Landscape);
 	suite_add_tcase(s, tc_core);
 
 	return s;
