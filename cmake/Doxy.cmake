0564581319e8e88f09638bf5096e6ca800fbbb2d
Ege Çetin <64282645+egecetin@users.noreply.github.com>
2022-10-29 22:01:51 +0300

    Add doxy (#6)

---
+++ cmake/Doxy.cmake
@@ -0,0 +1,21 @@
+# From https://stackoverflow.com/questions/60955881/cmake-for-doxygen
+
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
+  # set input and output files
+  set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/doc/Doxyfile.in)
+  set(DOXYGEN_OUT ${PROJECT_SOURCE_DIR}/Doxyfile)
+
+  # request to configure the file
+  configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+
+  # note the option ALL which allows to build the docs together with the application
+  add_custom_target(
+    docs
+    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMENT "Generating API documentation with Doxygen"
+    VERBATIM)
+else()
+  message("${BoldYellow}Doxygen need to be installed to generate the doxygen documentation!${ColourReset}")
+endif()
