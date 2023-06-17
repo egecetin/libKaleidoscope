642381b02ea0842d728d438331ff8e48bdbf19c3
egecetin <64282645+egecetin@users.noreply.github.com>
2023-06-17 19:49:10 +0000

    Deploying to gh-pages from @ egecetin/libKaleidoscope@65b2c817c94085fa30c6d7762c26612dc8bdbd72 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['ncomponents_19',['nComponents',['../structKaleidoscopeHandle__t.html#ac2eeae2cb291eb691a9546a4af805ec3',1,'KaleidoscopeHandle_t']]],
-  ['npoints_20',['nPoints',['../structKaleidoscopeHandle__t.html#a47925f2b1a4806e8449e661919c79d73',1,'KaleidoscopeHandle_t']]]
+  ['npoints_20',['nPoints',['../structKaleidoscopeHandle__t.html#a86f503a3e8c0cf40ddbfa7faf2c10ff0',1,'KaleidoscopeHandle_t']]]
 ];
